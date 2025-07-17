22d75fb830c918c483fa6f12e67456dcb10f4e64
QuangPV10 <phamvanquang.work@gmail.com>
2025-07-17 23:09:04 +0700

    Remove output block for website URL from Terraform configuration

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,10 @@
-output "website_url" {
-  description = "The URL of the static website."
-  value       = aws_s3_bucket_website_configuration.website_config.website_endpoint
-}
-
 output "cloudfront_domain_name" {
   description = "The domain name of the CloudFront distribution."
   value       = aws_cloudfront_distribution.s3_distribution.domain_name
 }
+
+# XÓA HOÀN TOÀN KHỐI OUTPUT "website_url" BÊN DƯỚI
+# output "website_url" {
+#  description = "The URL of the static website."
+#  value       = aws_s3_bucket_website_configuration.website_config.website_endpoint
+# }
