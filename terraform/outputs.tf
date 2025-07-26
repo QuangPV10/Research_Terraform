82e3186f483d3ba1b88469963a12ee31b273c200
QuangPV10 <phamvanquang.work@gmail.com>
2025-07-26 16:49:07 +0700

    Remove output block for website URL and ensure CloudFront distribution ID output is defined

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,6 +3,11 @@ output "cloudfront_domain_name" {
   value       = aws_cloudfront_distribution.s3_distribution.domain_name
 }
 
+output "cloudfront_distribution_id" {
+  description = "The ID of the CloudFront distribution."
+  value       = aws_cloudfront_distribution.s3_distribution.id
+}
+
 # XÓA HOÀN TOÀN KHỐI OUTPUT "website_url" BÊN DƯỚI
 # output "website_url" {
 #  description = "The URL of the static website."
